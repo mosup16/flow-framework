e27baf849956aef65d5e46e775a2a7054d4bdaeb
mosup emad <mosup16@gmail.com>
2022-10-02 00:07:44 +0200

    test singular channel push() method

--- src/test/java/com/mo16/flow/SingularMessageChannelTest.java
+++ src/test/java/com/mo16/flow/SingularMessageChannelTest.java
@@ -1,6 +1,11 @@
 package com.mo16.flow;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 class SingularMessageChannelTest {
@@ -8,4 +13,17 @@ class SingularMessageChannelTest {
     @BeforeEach
     void setUp() {
     }
+
+
+    @Test
+    @DisplayName("test message push to a singular channel")
+    void push() {
+        SingularMessageChannel<Integer> channel = new SingularMessageChannel<>();
+        SequentialStep<Integer,Integer> step = mock(new SequentialStep<Integer, Integer>(){}.getClass());
+        channel.setSubscriber(step);
+        channel.push(1);
+        assertEquals(1, channel.poll());
+        verify(step, times(1)).startPolling();
+    }
+
 }
