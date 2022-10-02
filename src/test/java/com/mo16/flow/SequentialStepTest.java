50b33a989f96b0f4468eea0067fc5599916e8b8b
mosup emad <mosup16@gmail.com>
2022-10-02 18:17:44 +0200

    create SequentialStepTest with empty test cases

---
+++ src/test/java/com/mo16/flow/SequentialStepTest.java
@@ -0,0 +1,24 @@
+package com.mo16.flow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SequentialStepTest {
+
+    @Test
+    void startPolling() {
+    }
+
+    @Test
+    void pollMessage() {
+    }
+
+    @Test
+    void channelClosed() {
+    }
+
+    @Test
+    void copy() {
+    }
+}
