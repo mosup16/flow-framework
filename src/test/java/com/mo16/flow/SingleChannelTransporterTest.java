4c58625eb48121bbf9c3a00e9565975340584821
mosup emad <mosup16@gmail.com>
2022-10-02 05:51:36 +0200

    test publishMessage() on SingleChannelTransporterTest.java

--- src/test/java/com/mo16/flow/SingleChannelTransporterTest.java
+++ src/test/java/com/mo16/flow/SingleChannelTransporterTest.java
@@ -1,6 +1,20 @@
 package com.mo16.flow;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
 class SingleChannelTransporterTest {
 
+    @Test
+    @DisplayName("test message publishing via SingleChannelTransporter")
+    void publishMessage() {
+        var transporter = new SingleChannelTransporter<Integer>();
+        Channel<Integer> channel = mock(Channel.class);
+        transporter.addChannel(channel);
+        transporter.publishMessage(1);
+        verify(channel, times(1)).push(1);
+    }
 
 }
